2a5a53e1d7e3716e92b9116203043ffcf0ad083b
afshaalzubair <afshaalzubair@outlook.com>
2022-03-23 16:26:29 -0500

    change transition duration

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ class CarbonomixApp(MDApp):
 
         def fade_in_text(dt=None):
             Animation(
-                opacity=1, duration=2.4
+                opacity=1, duration=0.75
             ).start(starting_screen.ids.title)
 
         if not DEBUG:
