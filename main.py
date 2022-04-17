a7fe8bde60f71063949226d41ba9ebf4e0a6aa7e
Kishen Misra <kishenjmisra@gmail.com>
2022-04-17 11:53:20 -0500

    pickers to picker

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from kivymd.uix.list import OneLineAvatarIconListItem
 from kivymd.uix.button import MDFlatButton, MDRaisedButton
 from kivymd.uix.snackbar import BaseSnackbar
 from kivymd.uix.datatables import MDDataTable
-from kivymd.uix.pickers import MDDatePicker
+from kivymd.uix.picker import MDDatePicker
 
 from database import close, update, query, create_tables, update_footprint, get_footprint, get_current_values, categories, category_names, category_value_formats
 
