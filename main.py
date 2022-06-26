e2727d5b40883fe6ac9997b490dcd13d9279f953
Name <64455663+1624252@users.noreply.github.com>
2022-06-26 10:55:37 -0500

    style: adjust formatting

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime, timedelta, date
 
-from programs import program_text
+from data import program_text, colors
 
 
 # DEBUG = True means you're testing.
@@ -38,71 +38,6 @@ week_interval = 7 * 24 * 60 * 60
 
 sm: ScreenManager
 plt.rcParams.update({'font.size': 8})
-
-colors = {
-    "Teal": {
-        "50": "e4f8f9",
-        "100": "bdedf0",
-        "200": "97e2e8",
-        "300": "79d5de",
-        "400": "6dcbd6",
-        "500": "6ac2cf",
-        "600": "63b2bc",
-        "700": "5b9ca3",
-        "800": "54888c",
-        "900": "486363",
-        "A100": "bdedf0",
-        "A200": "97e2e8",
-        "A400": "6dcbd6",
-        "A700": "5b9ca3",
-    },
-    "Blue": {
-        "50": "e3f3f8",
-        "100": "b9e1ee",
-        "200": "91cee3",
-        "300": "72bad6",
-        "400": "62acce",
-        "500": "589fc6",
-        "600": "5191b8",
-        "700": "487fa5",
-        "800": "426f91",
-        "900": "35506d",
-        "A100": "b9e1ee",
-        "A200": "91cee3",
-        "A400": "62acce",
-        "A700": "487fa5",
-    },
-    "Red": {
-        "50": "FFEBEE",
-        "100": "FFCDD2",
-        "200": "EF9A9A",
-        "300": "E57373",
-        "400": "EF5350",
-        "500": "F44336",
-        "600": "E53935",
-        "700": "D32F2F",
-        "800": "C62828",
-        "900": "B71C1C",
-        "A100": "FF8A80",
-        "A200": "FF5252",
-        "A400": "FF1744",
-        "A700": "D50000",
-    },
-    "Light": {
-        "StatusBar": "E0E0E0",
-        "AppBar": "F5F5F5",
-        "Background": "FAFAFA",
-        "CardsDialogs": "FFFFFF",
-        "FlatButtonDown": "cccccc",
-    },
-    "Dark": {
-        "StatusBar": "000000",
-        "AppBar": "212121",
-        "Background": "303030",
-        "CardsDialogs": "424242",
-        "FlatButtonDown": "999999",
-    }
-}
 
 
 class StartingScreen(Screen):
